de452e53ee33af6e87fa2a70d36ecba0c80587ba
Upendra Roul <roul@adobe.com>
2020-11-25 23:41:31 +0530

    Improving Burger Project

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import Button from '../../../components/UI/Button/Button';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
     state = {
@@ -113,49 +114,16 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token);
     }
 
-    checkValidity = (value, rules) => {
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
-        
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid;
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid;
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm
-        }; // The nested objects are stil in mutable state
-        const  updatedFormEleemnt = {
-            ...updatedOrderForm[inputIdentifier]
-        }; // to make it immutable we are again using the child object in spread operator
-
-        updatedFormEleemnt.value = event.target.value;
-        updatedFormEleemnt.valid = this.checkValidity(updatedFormEleemnt.value, updatedFormEleemnt.validation);
-        updatedFormEleemnt.touched = true;
-        updatedOrderForm[inputIdentifier] = updatedFormEleemnt;
+        
+        const  updatedFormEleemnt = updateObject(this.state.orderForm[inputIdentifier], {
+            value : event.target.value,
+            valid : checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            touched : true
+        });
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier]: updatedFormEleemnt
+        });
 
         let formIsValid = true;
         for (let inputIdentifier in updatedOrderForm) {
